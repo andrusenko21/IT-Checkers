30f0ad2a90c4a0a3a5137bc9652d62f89a84b5b3
Max <Max-der-Monarch@yandex.ua>
2018-05-06 19:34:52 +0300

    Adds game constructor and initializes a game by making white player be a current player in the start of the game

--- Checkers/Checkers/Game.cs
+++ Checkers/Checkers/Game.cs
@@ -8,6 +8,24 @@ namespace Checkers
 {
     class Game
     {
+        public Board GameBoard { get; set; }
+        public IPlayer WhitePlayer { get; set; }
+        public IPlayer BlackPlayer { get; set; }
+        private IPlayer CurrentPlayer;
+        public Game(IPlayer whitePlayer, IPlayer blackPlayer, Board gameBoard)
+        {
+            if (whitePlayer == null || blackPlayer == null || gameBoard == null) throw new ArgumentException("Null arguments passed to Game constructor.");
 
+            Init(whitePlayer, blackPlayer, gameBoard);
+        }
+        private void Init(IPlayer whitePlayer, IPlayer blackPlayer, Board gameBoard)
+        {
+            WhitePlayer = whitePlayer;
+            BlackPlayer = blackPlayer;
+              
+            GameBoard = gameBoard;
+
+            CurrentPlayer = whitePlayer;
+        }
     }
 }
