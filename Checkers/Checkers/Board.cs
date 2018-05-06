e5922686b5ff2e7b3b535c35a55a9782eae72bc9
Max <Max-der-Monarch@yandex.ua>
2018-05-06 22:16:48 +0300

    Starts working on board displaying feature. Displaying initial view of the board

--- Checkers/Checkers/Board.cs
+++ Checkers/Checkers/Board.cs
@@ -8,5 +8,30 @@ namespace Checkers
 {
     class Board : List<List<string>>
     {
+        public Board()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (i < 3) Add(BuildInitialRow("X", i));
+                if (i > 2 && i < 5) Add(BuildInitialRow(".", i));
+                if (i > 4) Add(BuildInitialRow("O", i));
+            }
+        }
+        private List<string> BuildInitialRow(string type, int rowNum)
+        {
+            string firstType = ".", secondType = ".";
+            if (rowNum % 2 == 0)
+            {
+                firstType = ".";
+                secondType = type;
+            }
+            else
+            {
+                firstType = type;
+                secondType = ".";
+            }
+
+            return new List<string>() { firstType, secondType, firstType, secondType, firstType, secondType, firstType, secondType };
+        }
     }
 }
