63705e293f9a212f2d2c3f86433aa0acceeabe86
andre9773 <andre199773@mail.ru>
2018-05-08 19:30:07 +0300

    Adds Unit-test for the GetValidMoves method from the Game class

--- Checkers/CheckersTests/GameTest.cs
+++ Checkers/CheckersTests/GameTest.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 
 namespace CheckersTests
 {
+
     [TestClass()]
     public class GameTest
     {
 
 
         private TestContext testContextInstance;
+
 
         public TestContext TestContext
         {
@@ -23,7 +25,6 @@ namespace CheckersTests
                 testContextInstance = value;
             }
         }
-
 
         [TestMethod()]
         public void GetRemainingPiecesTest()
@@ -39,7 +40,6 @@ namespace CheckersTests
             actual = target.GetRemainingPieces(color);
             Assert.AreEqual(expected, actual);
         }
-
 
         [TestMethod()]
         public void DoMoveTestForJumpUpdateDownRight()
@@ -113,6 +113,103 @@ namespace CheckersTests
             Assert.IsTrue(target.GameBoard[3][2] == ".");
             Assert.IsTrue(target.GameBoard[2][3] == "O");
             Assert.IsTrue(target.GameBoard[4][1] == ".");
+        }
+
+
+        [TestMethod()]
+        public void GetValidMovesTestORegular()
+        {
+            IPlayer whitePlayer = new ComputerPlayer();
+            IPlayer blackPlayer = new ComputerPlayer();
+
+            Board gameBoard = TestHelper.CreateEmptyBoard();
+            gameBoard[4][1] = "O";
+            Game target = new Game(whitePlayer, blackPlayer, gameBoard);
+
+            List<Move> actual = target.GetValidMovesRemaining();
+            var fromLocation = new Tuple<int, int>(4, 1);
+
+            var expected = new List<Move>()
+            {
+                new Move{ From = fromLocation, To = new Tuple<int, int>(3, 0), IsJump = false},
+                new Move{ From = fromLocation, To = new Tuple<int, int>(3, 2), IsJump = false}
+            };
+        }
+
+
+        [TestMethod()]
+        public void GetValidMovesTestOJump()
+        {
+            IPlayer whitePlayer = new ComputerPlayer();
+            IPlayer blackPlayer = new ComputerPlayer();
+
+            Board gameBoard = TestHelper.CreateEmptyBoard();
+            gameBoard[2][1] = "X";
+            gameBoard[3][2] = "O";
+
+            Game target = new Game(whitePlayer, blackPlayer, gameBoard);
+
+            List<Move> actual = target.GetValidMovesRemaining();
+            var fromLocation = new Tuple<int, int>(3, 2);
+
+            var expected = new List<Move>()
+            {
+                new Move{ From = fromLocation, To = new Tuple<int, int>(2, 3), IsJump = false},
+                new Move{ From = fromLocation, To = new Tuple<int, int>(0, 0), IsJump = true}
+            };
+        }
+
+
+        [TestMethod()]
+        public void GetValidMovesTestXRegular()
+        {
+            IPlayer whitePlayer = new ComputerPlayer();
+            IPlayer blackPlayer = new ComputerPlayer();
+
+            Board gameBoard = TestHelper.CreateEmptyBoard();
+            gameBoard[4][1] = "X";
+            Game target = new Game(whitePlayer, blackPlayer, gameBoard);
+
+            SwitchGamePlayer(target);
+
+            List<Move> actual = target.GetValidMovesRemaining();
+            var fromLocation = new Tuple<int, int>(4, 1);
+
+            var expected = new List<Move>()
+            {
+                new Move{ From = fromLocation, To = new Tuple<int, int>(5, 0), IsJump = false},
+                new Move{ From = fromLocation, To = new Tuple<int, int>(5, 2), IsJump = false}
+            };
+        }
+
+
+        [TestMethod()]
+        public void GetValidMovesTestXJump()
+        {
+            IPlayer whitePlayer = new ComputerPlayer();
+            IPlayer blackPlayer = new ComputerPlayer();
+
+            Board gameBoard = TestHelper.CreateEmptyBoard();
+            gameBoard[2][1] = "X";
+            gameBoard[3][2] = "O";
+
+            Game target = new Game(whitePlayer, blackPlayer, gameBoard);
+
+            SwitchGamePlayer(target);
+
+            List<Move> actual = target.GetValidMovesRemaining();
+            var fromLocation = new Tuple<int, int>(2, 1);
+
+            var expected = new List<Move>()
+            {
+                new Move{ From = fromLocation, To = new Tuple<int, int>(3, 0), IsJump = false},
+                new Move{ From = fromLocation, To = new Tuple<int, int>(4, 3), IsJump = true}
+            };
+        }
+
+        private void SwitchGamePlayer(Game game)
+        {
+            game.DoMove(new Move() { From = new Tuple<int, int>(0, 0), To = new Tuple<int, int>(0, 0), IsJump = false });
         }
     }
 }
