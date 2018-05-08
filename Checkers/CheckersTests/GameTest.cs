d7f9eee9a9f555565d04fb006c8610f0ea3d0e8a
andre9773 <andre199773@mail.ru>
2018-05-08 19:17:02 +0300

    Adds Unit-test for the DoMove method from the Game class

--- Checkers/CheckersTests/GameTest.cs
+++ Checkers/CheckersTests/GameTest.cs
@@ -40,5 +40,79 @@ namespace CheckersTests
             Assert.AreEqual(expected, actual);
         }
 
+
+        [TestMethod()]
+        public void DoMoveTestForJumpUpdateDownRight()
+        {
+            IPlayer whitePlayer = new ComputerPlayer();
+            IPlayer blackPlayer = new ComputerPlayer();
+            Board gameBoard = TestHelper.CreateEmptyBoard();
+            gameBoard[2][1] = "O";
+            gameBoard[3][2] = "X";
+            Game target = new Game(whitePlayer, blackPlayer, gameBoard);
+            Move move = new Move() { From = new Tuple<int, int>(2, 1), To = new Tuple<int, int>(4, 3), IsJump = true };
+
+            target.DoMove(move);
+
+            Assert.IsTrue(target.GameBoard[3][2] == ".");
+            Assert.IsTrue(target.GameBoard[2][1] == ".");
+            Assert.IsTrue(target.GameBoard[4][3] == "O");
+        }
+
+        [TestMethod()]
+        public void DoMoveTestForJumpUpdateDownLeft()
+        {
+            IPlayer whitePlayer = new ComputerPlayer();
+            IPlayer blackPlayer = new ComputerPlayer();
+            Board gameBoard = TestHelper.CreateEmptyBoard();
+            gameBoard[2][3] = "O";
+            gameBoard[3][2] = "X";
+            Game target = new Game(whitePlayer, blackPlayer, gameBoard);
+            Move move = new Move() { From = new Tuple<int, int>(2, 3), To = new Tuple<int, int>(4, 1), IsJump = true };
+
+            target.DoMove(move);
+
+            Assert.IsTrue(target.GameBoard[2][3] == ".");
+            Assert.IsTrue(target.GameBoard[3][2] == ".");
+            Assert.IsTrue(target.GameBoard[4][1] == "O");
+        }
+
+        [TestMethod()]
+        public void DoMoveTestForJumpUpdateUpLeft()
+        {
+            IPlayer whitePlayer = new ComputerPlayer();
+            IPlayer blackPlayer = new ComputerPlayer();
+
+            Board gameBoard = TestHelper.CreateEmptyBoard();
+            gameBoard[4][3] = "O";
+            gameBoard[3][2] = "X";
+            Game target = new Game(whitePlayer, blackPlayer, gameBoard);
+            Move move = new Move() { From = new Tuple<int, int>(4, 3), To = new Tuple<int, int>(2, 1), IsJump = true };
+
+            target.DoMove(move);
+
+            Assert.IsTrue(target.GameBoard[3][2] == ".");
+            Assert.IsTrue(target.GameBoard[2][1] == "O");
+            Assert.IsTrue(target.GameBoard[4][3] == ".");
+        }
+
+        [TestMethod()]
+        public void DoMoveTestForJumpUpdateUpRight()
+        {
+            IPlayer whitePlayer = new ComputerPlayer();
+            IPlayer blackPlayer = new ComputerPlayer();
+
+            Board gameBoard = TestHelper.CreateEmptyBoard();
+            gameBoard[4][1] = "O";
+            gameBoard[3][2] = "X";
+            Game target = new Game(whitePlayer, blackPlayer, gameBoard);
+            Move move = new Move() { From = new Tuple<int, int>(4, 1), To = new Tuple<int, int>(2, 3), IsJump = true };
+
+            target.DoMove(move);
+
+            Assert.IsTrue(target.GameBoard[3][2] == ".");
+            Assert.IsTrue(target.GameBoard[2][3] == "O");
+            Assert.IsTrue(target.GameBoard[4][1] == ".");
+        }
     }
 }
