bcd9e95c690e497e10ec671d1c45492068452499
Positivist <Max-der-Monarch@yandex.ua>
2018-05-08 16:36:20 +0300

    Adds method to HumanPlayer that gives possibility to choose color of checkers

--- Checkers/Checkers/HumanPlayer.cs
+++ Checkers/Checkers/HumanPlayer.cs
@@ -9,6 +9,22 @@ namespace Checkers
     class HumanPlayer : IPlayer
     {
         public string PieceSymbol { get; set; }
+        public string PromptForColor()
+        {
+            Console.Write("Which color do you want to be? Black or White? [B][W]: ");
+            string colorChoice = Console.ReadLine();
+
+            if (colorChoice.ToUpper() != "B" && colorChoice.ToUpper() != "W")
+            {
+                Console.WriteLine("Please pick either B or W for a color choice.");
+                Console.WriteLine();
+                colorChoice = PromptForColor();
+            }
+
+            PieceSymbol = (colorChoice.ToUpper() == "W") ? "O" : "X";
+
+            return colorChoice;
+        }
         public Move GetMove(List<Move> validMoves, Board gameBoard)
         {
             return new Move();
