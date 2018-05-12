2cfd9707f53bf25e4279113a94f309fba054eec3
Positivist <Max-der-Monarch@yandex.ua>
2018-05-12 16:18:27 +0300

    LW5^ Moq.dll

---
+++ Checkers/Checkers/IFormBoard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Checkers
+{
+    interface IFormBoard
+    {
+        string getColorOnPosition(Tuple<int, int> coord);
+        List<string> this[int x] { get; set; }
+    }
+}
